70231e8249092b8046bbf679276110b81e5a5ca7
bredele <olivier.wietrich@gmail.com>
2014-11-01 20:22:31 -0600

    add documentation

---
+++ index.js
@@ -0,0 +1,24 @@
+
+/**
+ * Expose 'walk'
+ */
+
+module.exports = walk;
+
+
+/**
+ * Walk dom tree recursively.
+ *
+ * It traverses dom elements
+ * (type 1) as well as text
+ * node (type 3).
+ * 
+ * @param {ELement} node
+ * @param {Function} cb
+ * @param {Object} scope
+ * @api public
+ */
+
+function walk() {
+  //do something
+}
